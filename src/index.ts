106cf7c075c959934770e2d85fd6a4a74fe6b3bd
TravelFiend <mikegrace274@gmail.com>
2020-10-16 15:31:08 -0400

    strings are now sortable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbers = new NumbersCollection([12, 4, -6, 389, 0])
-const sorter = new Sorter(numbers);
-sorter.sort();
+const characters = new CharactersCollection('ChwjrDhdjkbQpxka');
+
+const sorter1 = new Sorter(numbers);
+const sorter2 = new Sorter(characters);
+
+sorter1.sort();
+sorter2.sort();
+
 console.log(numbers.data);
+console.log(characters.data);
