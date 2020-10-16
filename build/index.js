da75ca16df750d779dd7aff2767c3d119cbec6e6
TravelFiend <mikegrace274@gmail.com>
2020-10-16 15:01:13 -0400

    refactor madness

--- build/index.js
+++ build/index.js
@@ -1,26 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Sorter = void 0;
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        // BUBBLE SORT
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-exports.Sorter = Sorter;
-var sorter = new Sorter([381, -432, 0, -143]);
+var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbers = new NumbersCollection_1.NumbersCollection([12, 4, -6, 389, 0]);
+var sorter = new Sorter_1.Sorter(numbers);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbers.data);
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,7 @@
-export class Sorter {
-  constructor(public collection: number[] | string) { }
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    // BUBBLE SORT
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-
-        // if collection is an array of numbers
-        if (this.collection instanceof Array) {     //  TYPE GUARD HERE
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-
-        // if collection is a string
-        if (typeof this.collection === 'string') {     //  TYPE GUARD HERE
-
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([381, -432, 0, -143]);
-
+const numbers = new NumbersCollection([12, 4, -6, 389, 0])
+const sorter = new Sorter(numbers);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbers.data);
