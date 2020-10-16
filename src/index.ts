12eb43a18f366ed034f52a04b8c11e1edebd1800
TravelFiend <mikegrace274@gmail.com>
2020-10-16 13:54:53 -0400

    type guards with less than great implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export class Sorter {
-  constructor(public collection: number[]) { }
+  constructor(public collection: number[] | string) { }
 
   sort(): void {
     const { length } = this.collection;
@@ -7,10 +7,19 @@ export class Sorter {
     // BUBBLE SORT
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+
+        // if collection is an array of numbers
+        if (this.collection instanceof Array) {     //  TYPE GUARD HERE
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+
+        // if collection is a string
+        if (typeof this.collection === 'string') {     //  TYPE GUARD HERE
+
         }
       }
     }
